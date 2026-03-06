55ade4872c3d7c61d34c4c2e99c6110cb04fd153
janushan12 <janu.1999.12@gmail.com>
2026-03-06 11:01:13 +0530

    Change arrow function to normal function

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,12 +7,13 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
-userSchema.pre('save', async (next) => {
+userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
-userSchema.methods.comparePassword = async () => {
+userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password)
 };
 
